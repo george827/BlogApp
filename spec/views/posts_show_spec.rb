a139c98982bcc2c1160fef5b60f0b157fae47929
Faranosh Amini <faranosh.amini4@gmail.com>
2023-04-11 21:41:01 +0430

    fix rubocop errors

--- spec/views/posts_show_spec.rb
+++ spec/views/posts_show_spec.rb
@@ -14,7 +14,6 @@ RSpec.describe 'root page features' do
         bio: 'Teacher from Poland.', posts_counter: 0
       )
     ]
-​
     @posts = [
       @post1 = Post.create(
         author: @user1,
@@ -31,31 +30,24 @@ RSpec.describe 'root page features' do
         likes_counter: 0
       )
     ]
-​
     visit user_post_path(@users.first, @posts.first)
   end
-​
   describe '#PostShowPage' do
     it 'Should display post Title' do
       expect(page).to have_content(@post1.Title)
     end
-​
     it 'should display the post author' do
       expect(page).to have_content(@posts.first.author.name)
     end
-​
     it 'should display the number of comments' do
       expect(page).to have_content(@posts.first.comments_counter)
     end
-​
     it 'should display the number of likes' do
       expect(page).to have_content(@posts.first.likes_counter)
     end
-​
     it 'should display the post body' do
       expect(page).to have_content(@posts.first.Text)
     end
-​
     it 'should display the name of the commentor' do
       @posts.first.comments.each do |comment|
         expect(page).to have_content(comment.author.name)
