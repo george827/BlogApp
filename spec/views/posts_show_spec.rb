13ba80adcf53156d1bd211ea7aa737c5c3c056db
george827 <kinyanjuigeorge827@gmail.com>
2023-04-11 20:11:42 +0300

    fix linter error

--- spec/views/posts_show_spec.rb
+++ spec/views/posts_show_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
-​
-RSpec.describe 'root page features' do
+
+RSpec.describe 'root page features', type: :feature do
   before(:each) do
     @users = [
       @user1 = User.create(
@@ -14,7 +14,7 @@ RSpec.describe 'root page features' do
         bio: 'Teacher from Poland.', posts_counter: 0
       )
     ]
-​
+
     @posts = [
       @post1 = Post.create(
         author: @user1,
@@ -31,37 +31,37 @@ RSpec.describe 'root page features' do
         likes_counter: 0
       )
     ]
-​
+
     visit user_post_path(@users.first, @posts.first)
   end
-​
+
   describe '#PostShowPage' do
     it 'Should display post Title' do
       expect(page).to have_content(@post1.Title)
     end
-​
+
     it 'should display the post author' do
       expect(page).to have_content(@posts.first.author.name)
     end
-​
+
     it 'should display the number of comments' do
       expect(page).to have_content(@posts.first.comments_counter)
     end
-​
+
     it 'should display the number of likes' do
       expect(page).to have_content(@posts.first.likes_counter)
     end
-​
+
     it 'should display the post body' do
       expect(page).to have_content(@posts.first.Text)
     end
-​
+
     it 'should display the name of the commentor' do
       @posts.first.comments.each do |comment|
         expect(page).to have_content(comment.author.name)
       end
     end
-​
+
     it 'should display each comment left by the commenter' do
       @posts.first.comments.each do |comment|
         expect(page).to have_content(comment.body)
